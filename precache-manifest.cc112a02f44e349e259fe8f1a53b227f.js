f47984304847ed8d970859c021f03c955323816c
DHvyas <vyasdhruvh@gmail.com>
2020-12-19 02:30:04 +0300

    Updates

---
+++ precache-manifest.cc112a02f44e349e259fe8f1a53b227f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57afefe97a885d246eb57996cc8b58b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c45e86cd84fdcfe1394",
+    "url": "/static/css/main.e214f93f.chunk.css"
+  },
+  {
+    "revision": "c802a7516f2efda5a3bd",
+    "url": "/static/js/2.0685c2ba.chunk.js"
+  },
+  {
+    "revision": "3c45e86cd84fdcfe1394",
+    "url": "/static/js/main.afa543ba.chunk.js"
+  },
+  {
+    "revision": "7c20b5d0c222780686f7",
+    "url": "/static/js/runtime-main.a7c448a7.js"
+  },
+  {
+    "revision": "4996346cd35a6d31c6bc5163cc2a1d31",
+    "url": "/static/media/Apercu-Light.4996346c.eot"
+  },
+  {
+    "revision": "a5229c50f1bd6fb1d2f1cadf7346e371",
+    "url": "/static/media/Apercu-Light.a5229c50.woff"
+  },
+  {
+    "revision": "411f1ccbe6a02ef58c20ac6e1f45a9ef",
+    "url": "/static/media/dtulogo.411f1ccb.png"
+  },
+  {
+    "revision": "90a564d3042c5708d0d6bb6fb864f0e5",
+    "url": "/static/media/jufmyrlogo.90a564d3.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0994f5b73c2a61da4842263dcbd7b367",
+    "url": "/static/media/tedxlogo.0994f5b7.png"
+  }
+]);
